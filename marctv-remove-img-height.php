a9f266649fc3bce74137b6752772fd34b1874e97
MarcDK <MarcDK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-04 16:12:41 +0000

    Tagging 1.2
    git-svn-id: https://plugins.svn.wordpress.org/marctv-remove-img-height/tags/1.2@979759 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- marctv-remove-img-height.php
+++ marctv-remove-img-height.php
@@ -1,19 +1,22 @@
 <?php
 /*
-  Plugin Name: MarcTV Remove img height
-  Plugin URI: http://www.marctv.de/blog/2010/08/25/marctv-wordpress-plugins/
+  Plugin Name: MarcTV Remove image height attribute
+  Plugin URI: http://www.marctv.de/blog/marctv-wordpress-plugins/
   Description: Removes the height attribute from all images in the content.
   Author: MarcDK
-  Version: 1.1
+  Version: 1.2
   Author URI: http://marctv.de
   License: GPL2
  */
 
 function remove_height_attribute($html) {
-  $html = preg_replace('#(<img.+?)height=(["\']?)\d*\2(.*?/?>)#i', '$1$3', $html);
+  $html = preg_replace('#(<img.+?)height=(["\']?)\d*\2(.*?/?>)#i', '$1$3', $html );
   return $html;
 }
 
 add_filter('the_content', 'remove_height_attribute');
+
+
+
 
 ?>
